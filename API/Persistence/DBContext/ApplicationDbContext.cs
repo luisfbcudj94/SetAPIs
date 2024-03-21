cae9a5e2e9f92d1778c44feb612ffd7aea95d385
luisfbcudj94 <alejandroabril98@gmail.com>
2024-03-21 15:26:10 -0500

    feat: Initial migration

---
+++ API/Persistence/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Domain.Models;
+
+namespace API.Persistence.DBContext
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<API.Domain.Models.API> APIs { get; set; }
+        public DbSet<APIType> APITypes { get; set; }
+        public DbSet<Body> Bodies { get; set; }
+        public DbSet<BodyTag> BodyTags { get; set; }
+        public DbSet<Configuration> Configurations { get; set; }
+        public DbSet<Header> Headers { get; set; }
+        public DbSet<HeaderTag> HeaderTags { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<URL> URLs { get; set; }
+        public DbSet<URLTag> URLTags { get; set; }
+
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,9 @@
 
+using API.Domain.Models;
+using API.Persistence.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace API
 {
     public class Program
@@ -7,12 +12,20 @@ namespace API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
