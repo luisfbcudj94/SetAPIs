a3a47aaf5f8b12f865926f8430e2a3f23d98780b
luisfbcudj94 <alejandroabril98@gmail.com>
2024-03-21 16:53:20 -0500

    feat: update type fields of Tag model

--- API/Domain/Models/Tag.cs
+++ API/Domain/Models/Tag.cs
@@ -3,7 +3,7 @@
     public class Tag
     {
         public virtual Guid Id { get; set; }
-        public virtual int Name { get; set; }
-        public virtual int Value { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Value { get; set; }
     }
 }
---
+++ API/Migrations/20240321215304_updateTypeFields-Tag-Model.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTypeFieldsTagModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Tags",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tags",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Value",
+                table: "Tags",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Tags",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
