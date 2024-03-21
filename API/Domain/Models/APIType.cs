fd56640f6a90e0d3b0bf625a903707d13d30138e
luisfbcudj94 <alejandroabril98@gmail.com>
2024-03-21 14:51:39 -0500

    feat: Create models

---
+++ API/Domain/Models/APIType.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Domain.Models
+{
+    public class APIType
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
---
+++ API/Domain/Models/URLTag.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Domain.Models
+{
+    public class URLTag
+    {
+        public virtual Guid Id { get; set; }
+        public virtual Guid UrlId { get; set; }
+        public virtual Guid TagId { get; set; }
+        public virtual Tag Tags { get; set; }
+
+    }
+}
